1710b65b23ca8f264a29e77bb42b209071f0eb85
Thomas Walsh <thomaseawalsh@gmail.com>
2021-06-04 22:37:08 +0100

    pre delete of eexisting class structure

--- root/js/main.js
+++ root/js/main.js
@@ -1,21 +1,20 @@
 const APIurl = 'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=1750&minDictionaryCount=5&minLength=3&maxLength=9&limit=50&api_key=ssueiy6cfj5zun3arboit5dht2xa81odz5k3iyrrr281zs73a'
 
-function getRandomWord () {
-    let wordsArr = fetch(APIurl)
-        .then (res => res.json())
-        .then (data => console.log(data));
-        return wordsArr
+function getWordsFromAPI () {
+    
 }
 
 let Word = class {
     constructor () {
         this.direction = Word.getDirection();
+        this.word = Word.getRandomWord();
     }
 
     static getRandomWord () {
-        let currentWord = getRandomWord()
-        currentWord = getRandomWord[0]
-        return currentWord.toUpperCase
+        let result = fetch(APIurl)
+        .then (res => res.json())
+        .then (data => console.log(data));
+        return result
     }
 
     static getDirection () {
@@ -33,8 +32,5 @@ let Word = class {
     }
 }
 
-let word1 = new Word 
-
+let word1 = new Word ()
 console.log (word1)
-
-
