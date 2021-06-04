3c8924fd6e1b7c2ebf7fcb5f36fc671684de69e5
Thomas Walsh <thomaseawalsh@gmail.com>
2021-06-04 15:07:40 +0100

    started to work on class constructor for words

--- root/js/main.js
+++ root/js/main.js
@@ -1,12 +1,40 @@
-function getWords () {
-    const url = 'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=2000&maxCorpusCount=-1&minDictionaryCount=25&maxDictionaryCount=-1&minLength=3&maxLength=9&limit=10&api_key=ssueiy6cfj5zun3arboit5dht2xa81odz5k3iyrrr281zs73a'
-    fetch(url)
+const APIurl = 'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=1750&minDictionaryCount=5&minLength=3&maxLength=9&limit=50&api_key=ssueiy6cfj5zun3arboit5dht2xa81odz5k3iyrrr281zs73a'
+
+function getRandomWord () {
+    let wordsArr = fetch(APIurl)
         .then (res => res.json())
-        .then (data => console.log(data))
-    
-    return data
+        .then (data => console.log(data));
+        return wordsArr
 }
 
-const words = getWords()
+let Word = class {
+    constructor () {
+        this.direction = Word.getDirection();
+    }
 
-console.log (words)
+    static getRandomWord () {
+        let currentWord = getRandomWord()
+        currentWord = getRandomWord[0]
+        return currentWord.toUpperCase
+    }
+
+    static getDirection () {
+        let value = Math.floor(Math.random()*4)
+        switch(value) {
+            case 0:
+            return "ttb"
+            case 1:
+            return "rtl"
+            case 2:
+            return "btt"
+            case 3:
+            return "ltr"
+        }
+    }
+}
+
+let word1 = new Word 
+
+console.log (word1)
+
+
