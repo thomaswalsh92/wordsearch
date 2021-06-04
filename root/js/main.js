b68f31432e602fb0a7ae7ea2162503e5fe29b42a
Thomas Walsh <thomaseawalsh@gmail.com>
2021-06-04 23:09:48 +0100

    created create word func that creates an array of word objects

--- root/js/main.js
+++ root/js/main.js
@@ -1,20 +1,7 @@
-const APIurl = 'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=1750&minDictionaryCount=5&minLength=3&maxLength=9&limit=50&api_key=ssueiy6cfj5zun3arboit5dht2xa81odz5k3iyrrr281zs73a'
-
-function getWordsFromAPI () {
-    
-}
-
 let Word = class {
-    constructor () {
+    constructor (word) {
         this.direction = Word.getDirection();
-        this.word = Word.getRandomWord();
-    }
-
-    static getRandomWord () {
-        let result = fetch(APIurl)
-        .then (res => res.json())
-        .then (data => console.log(data));
-        return result
+        this.word = word
     }
 
     static getDirection () {
@@ -32,5 +19,14 @@ let Word = class {
     }
 }
 
-let word1 = new Word ()
-console.log (word1)
+let wordNames = ['apple', 'banana', 'apricot', 'kiwi', 'strawberry']
+
+// takes an array and creates 
+function createWords (arr) {
+    let result = []
+    for (let e in arr) {
+        result.push (new Word (arr[e]))
+    }
+    return result
+}
+
